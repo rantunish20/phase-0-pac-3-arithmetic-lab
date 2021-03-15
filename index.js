8c9f5bd9ac4cebbb577777dd3688341985e2844e
Romel <rantunish20@gmail.com>
2021-03-14 22:31:37 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(a,b){
+    let total = a + b;
+    return total;
+}
+add()
+function subtract(a,b) {
+    let total = a - b;
+    return total;
+}
+subtract()
+function multiply(a,b) {
+    let total = a * b;
+    return total;
+}
+multiply()
+function divide(a,b) {
+    let total = a / b;
+    return total
+}
+divide()
+function increment(n) {
+    return n += 1
+}
+increment()
+function decrement(n) {
+    return n -= 1
+}
+decrement()
+function makeInt(n) {
+    return parseInt(n,10)
+}
+makeInt()
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
+preserveDecimal()
